934c238cb74a53d15ec4d8cd5214e01a6020d13b
steveparker123 <176498134+steveparker123@users.noreply.github.com>
2025-11-25 04:09:12 -0800

    Update: Auth text

--- src/app/auth/layout.jsx
+++ src/app/auth/layout.jsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }) => {
   return (
-    <div className="w-screen min-h-screen overflow-x-hidden flex justify-center items-center auth_bg p-3 md:py-8 backgroundImage">
+    <div className="w-screen min-h-screen overflow-x-hidden flex justify-center items-center p-3 md:py-8 backgroundImage">
       {children}
     </div>
   );
